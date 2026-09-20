682082bebe616780b1dc228473a05072ae95a59f
maruko92188 <maruko.kazuki.f@gmail.com>
2026-09-20 18:29:01 +0900

    mentionsテーブルを作成

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,11 @@ module BooksApp
     # config.eager_load_paths << Rails.root.join("extras")
 
     config.time_zone = 'Tokyo'
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.test_framework nil
+      g.jbuilder false
+    end
   end
 end
